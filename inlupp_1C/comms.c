ed101cdd684804a19cb71ce932c01a77aebcd15a
Conny Hansson <conhan@hotmail.se>
2020-10-27 18:10:38 +0100

    More comm work

--- inlupp_1C/comms.c
+++ inlupp_1C/comms.c
@@ -10,6 +10,22 @@ void remote_open_door(STATE* SYSTEM_STATE)
 
     SerialWritePort(
         SYSTEM_STATE->port,
+        SYSTEM_STATE->cmds[OPENDOOR],
+        BUFFERSIZE
+    );
+}
+bool send_card(Card* card, SERIALPORT* port, char* commands[])
+{
+    SerialWritePort(
+        *port,
+        commands[ADDCARD],
+        BUFFERSIZE
+    );
+}
+bool clear_cards(STATE* SYSTEM_STATE)
+{
+    SerialWritePort(
+        SYSTEM_STATE->port,
         SYSTEM_STATE->cmds[CLEARCARDS],
         BUFFERSIZE
     );
--- inlupp_1C/comms.h
+++ inlupp_1C/comms.h
@@ -2,3 +2,5 @@
 #include "state.h"
 
 void remote_open_door(STATE* SYSTEM_STATE);
+bool send_card(Card* card, SERIALPORT* port, char* commands[]);
+bool clear_cards(STATE* SYSTEM_STATE);
