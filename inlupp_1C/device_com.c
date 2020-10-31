1568e8abb729b01fa740f6497e7d7ed812750244
Conny Hansson <conhan@hotmail.se>
2020-10-31 13:41:44 +0100

    fixed comms

--- inlupp_1C/device_com.c
+++ inlupp_1C/device_com.c
@@ -5,67 +5,61 @@ bool remote_open_door(STATE* SYSTEM_STATE)
     // sends open door command to device
 
 
-    char command[2];
+    char message[2];
 
-    // convert command to string
-    //_itoa(OPENDOOR, command, 10);
-    command[COMMAND_INDEX] = OPENDOOR;
+    // add command to message
+    message[COMMAND_INDEX] = OPENDOOR;
+    // close message
+    message[COMMAND_INDEX + 1] = '\0';
     
     // sends command string to device
     return SerialWritePort(
         SYSTEM_STATE->port,
-        command,
-        sizeof(command)
+        message,
+        sizeof(message)
     );
 }
-void create_message(unsigned char* message_string, Card* card)
+void create_message(unsigned char message_string[], Card* card)
 {
-    // concatenates command and card ID
-    char command[2]; 
-    char delim[] = ".";
-
-    unsigned char id_byte = 0;
-    unsigned char converted_tag[5];
+    // formats message with add card command and card ID
     
 
-    char ID_tag[STRLEN];
-    strcpy(ID_tag, card->ID);
+    unsigned char id_byte = 0;
+    
+    // copy of card ID so original is untouched by strtok
+    char ID_copy[STRLEN];
+    strcpy(ID_copy, card->ID);
 
     // places command at front of message string
     message_string[COMMAND_INDEX] = ADDCARD;
 
-    // converts and places uid tag into converted_tag string
-    char* digit = strtok(ID_tag, delim);
+    // converts and places uid tag onto message
+    char delim[] = ".";
+    char* digit = strtok(ID_copy, delim);
 
-    int index = 0;
+    int index = 1;
     while (digit)
     {
         id_byte = atoi(digit);
-        converted_tag[index] = id_byte;
+        message_string[index] = (unsigned char)id_byte;
         digit = strtok(NULL, delim);
         index++;
     }
-
-    // adds card uid onto message string
-    strcat(message_string, converted_tag);
-    printf("\nmessage to send: %s", message_string);
-    for (int index = 0; index < strlen(message_string); index++)
-        printf("\nvalue %d: %d",index, message_string[index]);
 }
 bool clear_cards(STATE* SYSTEM_STATE)
 {
     // clears stored cards on device
 
-    char command[2];
+    char message[2];
     
-    // convert command to string 
-    //_itoa(CLEARCARDS, command, 10);
-    command[COMMAND_INDEX] = CLEARCARDS;
-
+    // add command to message
+    message[COMMAND_INDEX] = CLEARCARDS;
+    // close message
+    message[COMMAND_INDEX + 1] = '\0';
     // send string
     return SerialWritePort(
         SYSTEM_STATE->port,
-        command,
-        sizeof(command)
+        message,
+        sizeof(message)
     );
 }
